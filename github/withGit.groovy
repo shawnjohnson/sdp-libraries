e39bd3b6d9adf50dd1a417356d572a556ecf00e4
steven-terrana <steventerrana@gmail.com>
2019-08-01 22:12:29 -0400

    Update withGit.groovy

--- github/withGit.groovy
+++ github/withGit.groovy
@@ -4,7 +4,7 @@
 */
 
 void call(Map args, body){
-
+  
   // check required parameters
   if (!args.url || !args.cred)
     error """
@@ -14,19 +14,26 @@ void call(Map args, body){
       cred: jenkins credential ID for github. (required)
       branch: branch in the repository to checkout. defaults to master. (optional)
     """
-
-  withCredentials([usernamePassword(credentialsId: args.cred, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-    repo = args.url.split("/").last() - ".git"
-    withEnv(["git_url_with_creds=${args.url.replaceFirst("://","://${USER}:${PASS}@")}"]) {
-      node {
-        sh "rm -rf ${repo}"
-        sh "set +x && git clone ${env.git_url_with_creds}"
-        dir(repo){
-          sh "git checkout ${args.branch ?: "master"}"
-          push = "push"
-          body.resolveStrategy = Closure.DELEGATE_FIRST
-          body.delegate = this
-          body.call()
+  
+  // concurrent usage of withGit against the same repository
+  // can result in a stale repository causing git push to 
+  // fail.  this lock makes it so that only 1 pipeline
+  // can interact with a given repository using this step 
+  // at a time 
+  lock(args.url){
+    withCredentials([usernamePassword(credentialsId: args.cred, passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+      repo = args.url.split("/").last() - ".git"
+      withEnv(["git_url_with_creds=${args.url.replaceFirst("://","://${USER}:${PASS}@")}"]) {
+        node {
+          sh "rm -rf ${repo}"
+          sh "set +x && git clone ${env.git_url_with_creds}"
+          dir(repo){
+            sh "git checkout ${args.branch ?: "master"}"
+            push = "push"
+            body.resolveStrategy = Closure.DELEGATE_FIRST
+            body.delegate = this
+            body.call()
+          }
         }
       }
     }
