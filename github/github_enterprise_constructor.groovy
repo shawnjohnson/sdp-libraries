a9a2c99abd96d6fd51628cfe33d9e4ee2772f767
Carlos OKieffe <okieffe_carlos@bah.com>
2019-09-20 16:38:24 -0400

    changed head count to >= 2 to account for squashed merges

--- github/github_enterprise_constructor.groovy
+++ github/github_enterprise_constructor.groovy
@@ -25,7 +25,7 @@ void call(Map context) {
         env.GIT_BUILD_CAUSE = sh (
           script: 'git rev-list HEAD --parents -1 | wc -w', // will have 2 shas if commit, 3 or more if merge
           returnStdout: true
-        ).trim().toInteger() > 2 ? "merge" : "commit"
+        ).trim().toInteger() >= 2 ? "merge" : "commit"
       }
 
       println "Found Git Build Cause: ${env.GIT_BUILD_CAUSE}"
