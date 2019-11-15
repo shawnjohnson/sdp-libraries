2a12bea8f738e6ab4dfb8f23e4b76d46a21ac152
Carlos OKieffe <okieffe_carlos@bah.com>
2019-11-14 20:29:55 -0500

    feat: trying original sh call

--- owasp_dep_check/static_dependency_check_analysis.groovy
+++ owasp_dep_check/static_dependency_check_analysis.groovy
@@ -68,17 +68,17 @@ def call() {
       fi
       """
 
-//        sh """
-//        docker run -v \$(pwd):/src -v \$(pwd)/${data_dir}:/usr/share/dependency-check/data -v \$(pwd)/${
-//          report_dir
-//        }:/report owasp/dependency-check --scan ${scan_target} --format "${report_format}" --project "OWASP_dependency_check" --out /report ${suppression ?: ''}
-//      """
-        def owasp_image = config.image ?: "owasp-dep-check"
-        def owasp_docker_args = "-v \$(pwd):/src -v \$(pwd)/${data_dir}:/usr/share/dependency-check/data -v \$(pwd)/${report_dir }:/report"
-        def owasp_command_args = "--scan ${scan_target} --format \"${report_format}\" --project \"OWASP_dependency_check\" --out /report ${suppression ?: ''}"
-        with_run_sdp_image(owasp_image, [args:owasp_docker_args, command:owasp_command_args ]){
-          sh("ls -R .")
-        }
+        sh """
+        docker run -v \$(pwd):/src -v \$(pwd)/${data_dir}:/usr/share/dependency-check/data -v \$(pwd)/${
+          report_dir
+        }:/report owasp/dependency-check --scan ${scan_target} --format "${report_format}" --project "OWASP_dependency_check" --out /report ${suppression ?: ''}
+      """
+//        def owasp_image = config.image ?: "owasp-dep-check"
+//        def owasp_docker_args = "-v \$(pwd):/src -v \$(pwd)/${data_dir}:/usr/share/dependency-check/data -v \$(pwd)/${report_dir }:/report"
+//        def owasp_command_args = "--scan ${scan_target} --format \"${report_format}\" --project \"OWASP_dependency_check\" --out /report ${suppression ?: ''}"
+//        with_run_sdp_image(owasp_image, [args:owasp_docker_args, command:owasp_command_args ]){
+//          sh("ls -R .")
+//        }
 
         stash "workspace"
         archiveArtifacts allowEmptyArchive: true, artifacts: "${report_dir}/**"
