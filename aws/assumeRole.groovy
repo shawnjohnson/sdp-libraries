5578a33e612d9130e9132ac8a4a058879262a9d9
Shawn Johnson <shawnjohnson@users.noreply.github.com>
2020-02-13 14:55:45 -0500

    Update assumeRole.groovy

--- aws/assumeRole.groovy
+++ aws/assumeRole.groovy
@@ -10,7 +10,9 @@ def call(){
 
     // get response with secret key 
     String credURI = System.getenv("AWS_CONTAINER_CREDENTIALS_RELATIVE_URI")
-    def response = readJSON(text: "curl http://169.254.170.2${credURI} ".execute().text)
+    String r = "curl http://169.254.170.2${credURI} ".execute().text
+    println "response = ${r}"
+    def response = readJSON(text: r)
 
     env.AWS_ACCESS_KEY_ID = response.AccessKeyId
     env.AWS_SECRET_ACCESS_KEY = response.SecretAccessKey
