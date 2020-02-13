5fd8c64fea3f0642f93e92a3f978fa0e449ab21d
Shawn Johnson <shawnjohnson@users.noreply.github.com>
2020-02-13 14:50:53 -0500

    Update login_to_registry.groovy

--- docker/login_to_registry.groovy
+++ docker/login_to_registry.groovy
@@ -6,7 +6,16 @@
 void call(){
   def (image_repo, image_repo_cred) = get_registry_info()
 
-  withCredentials([usernamePassword(credentialsId: image_repo_cred, passwordVariable: 'pass', usernameVariable: 'user')]) {
-    sh "echo ${pass} | docker login -u ${user} --password-stdin ${image_repo}"
+  if(config.containsKey("ecr")){
+    inside_sdp_image "aws", {
+      assumeRole() 
+    }
+    
+    sh "$(aws ecr get-login --no-include-email)"
+    
+  } else {    
+    withCredentials([usernamePassword(credentialsId: image_repo_cred, passwordVariable: 'pass', usernameVariable: 'user')]) {
+      sh "echo ${pass} | docker login -u ${user} --password-stdin ${image_repo}"
+    }
   }
 }
