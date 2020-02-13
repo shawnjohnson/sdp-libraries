1873f6f018fe31383374d364130366cd100b6f01
Shawn Johnson <shawnjohnson@users.noreply.github.com>
2020-02-13 14:45:41 -0500

    Update get_registry_info.groovy

--- docker/get_registry_info.groovy
+++ docker/get_registry_info.groovy
@@ -3,9 +3,12 @@ def call() {
     errors = []
     ret << (config.registry ?:
             { errors << "Application Docker Image Registry, libraries.docker.registry, not defined in pipeline config" }() )
-    ret << ( config.cred ?:
-            { errors << "Application Docker Image Registry Credential, libraries.docker.cred, not defined in pipeline config" }() )
-
+    
+    if(!config.containsKey("ecr")){
+        ret << ( config.cred ?:
+                { errors << "Application Docker Image Registry Credential, libraries.docker.cred, not defined in pipeline config" }() )
+    }
+    
     if(!errors.empty) {
         error errors.join("; ")
     }
